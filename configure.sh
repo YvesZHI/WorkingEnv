ef9e02acab742fee9bd4b3b8d0670e2797123ca7
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-11 14:14:09 -0500

    init

---
+++ configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+source ./setDBUS.sh
+
+gsettings set org.gnome.Vino enabled true	
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino use-alternative-port true
+gsettings set org.gnome.Vino use-upnp true
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino alternative-port 5901
+
