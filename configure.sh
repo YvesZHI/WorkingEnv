173e4ce31b155c45f7b7c8fdfe016b14bad28e5a
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-12 17:18:10 +0800

    remove sudo

--- configure.sh
+++ configure.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 
-
 source ./setDBUS.sh
 
-res=$(gsettings set org.gnome.Vino enabled true 2>&1 >/dev/null)
-if [ -n "$res" ]; then
-	echo
-	echo "ERROR: do NOT run me as root or with sudo!"
-	echo
-	exit 1
-fi
+#res=$(gsettings set org.gnome.Vino enabled true 2>&1 >/dev/null)
+#echo $res
+#if [ -n "$res" ]; then
+#	echo
+#	echo "ERROR: do NOT run me as root or with sudo!"
+#	echo
+#	exit 1
+#fi
+gsettings set org.gnome.Vino enabled true 
 gsettings set org.gnome.Vino prompt-enabled false
 gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino use-alternative-port true
