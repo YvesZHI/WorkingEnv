ca9a3f45c6df6365c6c09a169052b1209e22f953
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-12 17:54:40 +0800

    debug

--- configure.sh
+++ configure.sh
@@ -3,15 +3,11 @@
 
 source ./setDBUS.sh
 
-#res=$(gsettings set org.gnome.Vino enabled true 2>&1 >/dev/null)
-#echo $res
-#if [ -n "$res" ]; then
-#	echo
-#	echo "ERROR: do NOT run me as root or with sudo!"
-#	echo
-#	exit 1
-#fi
-gsettings set org.gnome.Vino enabled true 
+res=$(gsettings set org.gnome.Vino enabled true 2>&1 >/dev/null)
+if [ -n "$res" ]; then
+	echo $res
+	exit 1
+fi
 gsettings set org.gnome.Vino prompt-enabled false
 gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino use-alternative-port true
