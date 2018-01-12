3197ae27e5dd81c0cb3ca7fc0aabfa7f8daf8001
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-12 06:33:59 -0500

    make port configurable

--- configure.sh
+++ configure.sh
@@ -13,7 +13,11 @@ gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino use-alternative-port true
 gsettings set org.gnome.Vino use-upnp true
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
-port=5901
+if [[ "$1" == "" ]]; then
+	port=5901
+else
+	port=$1
+fi
 gsettings set org.gnome.Vino alternative-port $port
 echo "$port is used to build remote desktop."
 echo "Please configure your viewer as x.x.x.x:$port"
