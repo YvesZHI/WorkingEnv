bfc7caba96adb54425ef2d574d0b008e917ad7a6
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-12 18:02:27 +0800

    set port

--- configure.sh
+++ configure.sh
@@ -13,5 +13,8 @@ gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino use-alternative-port true
 gsettings set org.gnome.Vino use-upnp true
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
-gsettings set org.gnome.Vino alternative-port 5901
+port=5901
+gsettings set org.gnome.Vino alternative-port $port
+echo "$port is used to build remote desktop."
+echo "Please configure your viewer as x.x.x.x:$port"
 vino-passwd
