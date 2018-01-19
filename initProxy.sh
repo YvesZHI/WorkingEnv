d65ab2a526e9e2f8a07c01e2ec5987a0fe75377b
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-20 03:25:34 +0800

    move exec bash to end

--- initProxy.sh
+++ initProxy.sh
@@ -21,7 +21,6 @@ echo "ftp_proxy=\$http_proxy" | tee -a ~/.bashrc > /dev/null
 echo "http_proxy=$http_proxy" | tee -a ~/.wgetrc > /dev/null
 echo "https_proxy=$http_proxy" | tee -a ~/.wgetrc > /dev/null
 echo "ftp_proxy=$http_proxy" | tee -a ~/.wgetrc > /dev/null
-exec bash  # source ~/.bashrc
 echo "Acquire::http::proxy \"$http_proxy\";" | sudo tee /etc/apt/apt.conf > /dev/null
 echo "Acquire::https::proxy \"${http_proxy/http/https}\";" | sudo tee -a /etc/apt/apt.conf > /dev/null
 echo "Acquire::https::proxy \"${http_proxy/http/https}\";" | sudo tee -a /etc/apt/apt.conf > /dev/null
@@ -33,3 +32,4 @@ echo "Acquire::BrokenProxy true;" | sudo tee -a /etc/apt/apt.conf > /dev/null
 my_ips=`ip -f inet addr|grep global | awk '{print $2}'|awk -F/ '{print $1}'|tr '\n' ','`
 host_names=`hostname -a | tr ' ' ','`
 export no_proxy=127.0.0.1,.huawei.com,localhost,local,.local,$my_ips,$host_names
+exec bash  # source ~/.bashrc
