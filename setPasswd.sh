84f151a2933c222ea3904f3588edd1989e74ca63
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-23 22:51:04 -1000

    enable to set passwd none

--- setPasswd.sh
+++ setPasswd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 
+if [[ "$1" == "none" ]]; then
+	gsettings set org.gnome.Vino authentication-methods "['none']"
+	exit 0
+fi
+
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 vino-passwd
