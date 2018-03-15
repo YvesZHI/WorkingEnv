44757b1a524b006f69635aa2d2044c5981f66a5a
YvesZHI <yuhui.zhi01@gmail.com>
2018-03-16 00:29:10 +0800

    add yes or not

--- config.sh
+++ config.sh
@@ -4,9 +4,13 @@
 sudo cp ca_huawei.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 
+read -p "configure proxy? [y/n]:" proxy
+if [[ "$proxy" == "Y" || "$proxy" == 'y' ]]; then
+	./initProxy.sh
+fi
+	
 ./config_ip.sh $1 $2
 
-./initProxy.sh
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
@@ -16,6 +20,9 @@ sudo cp -r ./ros /etc/
 
 sudo sed -i -e 's/hk/cn/g' /etc/apt/sources.list
 
-sudo adduser mdc
-sudo adduser mdc sudo
+read -p "add user mdc? [y/n]:" user
+if [[ "$proxy" == "Y" || "$proxy" == 'y' ]]; then
+	sudo adduser mdc
+	sudo adduser mdc sudo
+fi
 
