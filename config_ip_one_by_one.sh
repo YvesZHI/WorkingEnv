713a0f5a370e1bb8fc849a6e61bfbacc9af9674f
YvesZHI <yuhui.zhi01@gmail.com>
2018-03-16 01:09:05 +0800

    config ip one by one

---
+++ config_ip_one_by_one.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+arr=($(ls /sys/class/net/))
+unset 'arr[${#arr[@]}-1]'
+
+for i in "${arr[@]}"; do
+	./config_ip.sh "$i" $1
+	if [ $? -eq 0 ]; then
+		exit 0
+	fi
+done
+echo "None of configuration of network interfaces succeeded."
