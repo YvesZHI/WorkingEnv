68d10d445d79b5742d64aa379d480542d4d9f273
YvesZHI <yuhui.zhi01@gmail.com>
2018-03-19 23:34:44 +0800

    config ip for 17.10

--- config_ip.sh
+++ config_ip.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 
-sudo cp interfaces_bak interfaces
-sudo sed -i -e "s/NAME/$1/g" interfaces
-sudo sed -i -e "s/IP/$2/g" interfaces
-sudo mv interfaces /etc/network/
-sudo /etc/init.d/networking restart
-sudo service ssh restart
+version=`lsb_release -r | awk '{print $2}'`
 
+if [ $version -eq "16.04" ];then
+	sudo cp interfaces_bak interfaces
+	sudo sed -i -e "s/NAME/$1/g" interfaces
+	sudo sed -i -e "s/IP/$2/g" interfaces
+	sudo mv interfaces /etc/network/
+	sudo /etc/init.d/networking restart
+else
+	sudo cp 01-netcfg.yaml_bak 01-netcfg.yaml
+	sudo sed -i -e "s/NAME/$1/g" 01-netcfg.yaml
+	sudo sed -i -e "s/IP/$2/g" 01-netcfg.yaml
+	sudo mv 01-netcfg.yaml /etc/netplan/
+	sudo netplan apply
+fi
+
+sudo service ssh restart
