1658e8af3b74234ac846ac61bdd56a2f4bc4e209
z00436880 <zhiyuhui@huawei.com>
2018-03-27 22:41:20 +0800

    source initProxy.sh

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@ sudo cp ca_huawei.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 read -p "configure proxy? [y/n]:" proxy
 if [[ "$proxy" == "Y" || "$proxy" == "y" ]]; then
-	./initProxy.sh
+	source initProxy.sh
 fi
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver-options http-proxy=$http_proxy --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
